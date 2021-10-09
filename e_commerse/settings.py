157f8ca7bd2d9d96ff16c6ddded5807f8b81b07c
Orkhan <orkglh.com>
2021-10-09 17:38:12 +0400

    this is second commit

--- e_commerse/settings.py
+++ e_commerse/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'category',
     'account',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
--- e_commerse/urls.py
+++ e_commerse/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('category.urls')),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 ]
 
 if settings.DEBUG:
